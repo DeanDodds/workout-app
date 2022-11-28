9c8c2cdd3ab96fd773092e1353555ce2e8a8a267
DeanDodds <92326699+DeanDodds@users.noreply.github.com>
2022-11-28 18:16:57 +0000

    added workout app models

--- workoutapp/workout/models.py
+++ workoutapp/workout/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Exercise(models.Model):
+    exercise_name = models.CharField(max_length=255)
+    exercise_description = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Workout(models.Model):
+    created = models.DateField(auto_now=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    Lastname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+
+class WorkoutItem(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
+    exercise = models.ForeignKey(Exercise, on_delete=models.PROTECT)
+    reps = models.PositiveSmallIntegerField()
+    sets = models.PositiveSmallIntegerField()
